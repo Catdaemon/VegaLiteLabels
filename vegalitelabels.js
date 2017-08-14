03c63479ab05642c926aba35a87c58b45346f2bd
Carl Downie <Catdaemon@users.noreply.github.com>
2017-08-14 15:02:07 +0100

    Added support for line/point charts

--- vegalitelabels.js
+++ vegalitelabels.js
@@ -1,42 +1,62 @@
 function AddVegaLiteLabels(graph)
+{
+    // Supported types and their offsets
+    var typeOffsets = {
+        "rect": 4,
+        "line": -8,
+        "point": -8
+    }
+    // Loop through the current level
+    for(var i=0; i < graph.length; ++i)
+    {
+        var node = graph[i];
+
+        if (node.marktype && (typeOffsets[node.marktype]))
         {
-            // Loop through the current level
-            for(var i=0; i < graph.length; ++i)
+            // Found an eligable mark
+            // Loop through and create labels
+            for(var x=0; x < node.items.length; ++x)
             {
-                var node = graph[i];
+                var item = node.items[x];
+                var datum = item.datum;
 
-                if (node.marktype && (node.marktype == "rect"))
+                // See if the datum has a label field
+                if (datum.Label)
                 {
-                    // Found an eligable mark
-                    // Loop through and create labels
-                    for(var x=0; x < node.items.length; ++x)
-                    {
-                        var item = node.items[x];
-                        var datum = item.datum;
+                    // Calculate correct position
+                    var offset = typeOffsets[node.marktype] ? typeOffsets[node.marktype] : 0;
+                    var posX = item.x + (item.width ? item.width / 2 : 0);
+                    var posY = item.y + (item.height ? item.height / 2 : 0) + offset;
 
-                        // See if the datum has a label field
-                        if (datum.Label)
-                        {
-                            // Calculate correct position
-                            var posX = item.x + (item.width / 2);
-                            var posY = item.y + (item.height / 2);
+                    // Add the label
+                    var textelement = d3.select(node._svg).append("text")
+                        .attr("x", posX)
+                        .attr("y", posY)
+                        .attr("font-family", "Verdana")
+                        .attr("font-size", 11)
+                        .attr("fill", "black")
+                        .attr("text-anchor", "middle")
+                        .text(datum.Label);
 
-                            // Add the label
-                            d3.select(node._svg).append("text")
-                                .attr("x", posX)
-                                .attr("y", posY + 4)
-                                .attr("font-family", "Verdana")
-                                .attr("font-size", 11)
-                                .attr("fill", "black")
-                                .attr("text-anchor", "middle")
-                                .text(datum.Label);
-                        }                        
-                    }
-                }
-                if (node.items)
-                {
-                    // Search deeper
-                    AddLabels(node.items);
-                }
+                    // Update chart when the mouse moves over it - for zooming/panning esp. line charts
+                    (function(item, textelement, offset) {
+                        document.body.addEventListener('mousemove', function(){
+                            var posX = item.x + (item.width ? item.width / 2 : 0);
+                            var posY = item.y + (item.height ? item.height / 2 : 0) + offset;
+
+                            textelement.attr("x", posX);
+                            textelement.attr("y", posY + 4)
+                        });
+                    })(item, textelement, offset);
+
+                    
+                }                        
             }
         }
+        if (node.items)
+        {
+            // Search deeper
+            AddVegaLiteLabels(node.items);
+        }
+    }
+}
